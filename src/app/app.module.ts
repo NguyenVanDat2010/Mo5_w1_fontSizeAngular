fad5f677284f5f2bc71fd34d7c3671b576b6fa83
Kira <datdhvinh@gmail.com>
2020-08-11 15:00:18 +0700

    Color picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 import { PetComponent } from './pet/pet.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeEditorComponent,
     PetComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule,
